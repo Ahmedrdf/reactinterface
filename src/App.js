e928e26a74ed7fc99801cf0c701d2bbd9781ad1c
ahmedrdf <ahmedrdf.mansour@gmail.com>
2022-05-07 00:46:44 +0100

    add search and dropDown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <BiArchive className="inline-block text-blue-300" /> Your Appointement
       </h1>
+      <Search />
     </div>
   );
 }
