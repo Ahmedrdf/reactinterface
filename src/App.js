2cd2b5c79e37598f45346fbe679b4139f4f379e6
ahmedrdf <ahmedrdf.mansour@gmail.com>
2022-05-08 02:11:01 +0100

    add useState hooks and condtional classes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BiCalendar, BiTrash } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointement from "./components/AddAppointement";
 import AppointementList from "./data.json";
+import AppointementInfo from "./components/AppointementInfo";
 
 function App() {
   return (
@@ -13,27 +14,8 @@ function App() {
       <AddAppointement />
       <Search />
       <ul className="divide-y divide-gray-200">
-        {AppointementList.map((ap) => (
-          <li className="px-3 py-3 flex items-start">
-            <button
-              type="button"
-              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <BiTrash />
-            </button>
-            <div className="flex-grow">
-              <div className="flex items-center">
-                <span className="flex-none font-medium text-2xl text-blue-500">
-                  petName
-                </span>
-                <span className="flex-grow text-right">aptDate</span>
-              </div>
-              <div>
-                <b className="font-bold text-blue-500">Owner:</b> ownerName
-              </div>
-              <div className="leading-tight">aptNotes</div>
-            </div>
-          </li>
+        {AppointementList.map((appointment) => (
+          <AppointementInfo key={appointment.id} appointement={appointment} />
         ))}
       </ul>
     </div>
